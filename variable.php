ee28b492e7822046867c275fb28758b56c810e49
Siddhant Kumar Rajput <94693595+Siddhant-Kumar-Rajput@users.noreply.github.com>
2024-06-16 12:13:31 +0530

    Add files via upload

---
+++ variable.php
@@ -0,0 +1,17 @@
+<?php 
+
+// create variable using a $ sign 
+
+$name="sid";
+echo $name;
+
+//line breaks using <br> with just echo inside ""
+echo "<br>";
+
+
+$a=10;
+echo $a*3;
+
+// to use <br> in a sentence use <br/>
+echo "<br/> this is the result";
+?>
