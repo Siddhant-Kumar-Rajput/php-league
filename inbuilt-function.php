17e7d9c7386699bec21f2395dff59ed1844368c5
Siddhant Kumar Rajput <94693595+Siddhant-Kumar-Rajput@users.noreply.github.com>
2024-06-19 20:40:11 +0530

    Add files via upload

---
+++ inbuilt-function.php
@@ -0,0 +1,12 @@
+<?php
+$a="siddhant";
+$b=10;
+$arr=[1,"sid","raj"];
+echo strlen($a)."<br>";
+var_dump($b);
+echo "<br>".date("d-m-Y")."<br>";
+echo is_string($a)."<br>";
+echo is_int($b)."<br>";
+echo rand()."<br>";
+echo substr($a,4);
+?>
