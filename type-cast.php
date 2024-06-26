bcb14c85b94082078cf8d773bfccb2c3e12ad691
Siddhant Kumar Rajput <94693595+Siddhant-Kumar-Rajput@users.noreply.github.com>
2024-06-26 14:22:07 +0530

    Add files via upload

---
+++ type-cast.php
@@ -0,0 +1,20 @@
+<?php
+$a=10;
+var_dump($a);
+echo "<br>";
+$a=(string)$a;
+var_dump($a);
+echo "<br>";
+$a=(float)$a;
+var_dump($a);
+echo "<br>";
+$a=(boolean)$a;
+var_dump($a);
+echo "<br>";
+$a=(array)$a;
+var_dump($a);
+echo "<br>";
+$a=(object)$a;
+var_dump($a);
+echo "<br>";
+?>
